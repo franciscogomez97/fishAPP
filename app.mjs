318f4978250766f5392d0d2e11c23c3f880f4995
Francisco Gómez <franciscogomez@vt-lab.com>
2024-04-15 09:43:09 +0200

    15/04/24 starting up

--- app.mjs
+++ app.mjs
@@ -1,5 +1,4 @@
 import express from 'express';
-import pool from './db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,12 +11,3 @@ app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 })
 
-app.get('/test-db', async (req, res) => {
-    try {
-        const { rows } = await pool.query('SELECT NOW()');
-        res.json(rows[0]);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({error: 'Error al conectarse a la base de datos'});
-    }
-});
