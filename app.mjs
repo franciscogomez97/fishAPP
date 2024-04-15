30b410c57ddf12b44bd3b0b5448b5b13a9a40dfd
Francisco Gómez <franciscogomez@vt-lab.com>
2024-04-15 11:39:01 +0200

    15/04

--- app.mjs
+++ app.mjs
@@ -1,8 +1,23 @@
 import express from 'express';
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
 
+// Database connection
+let db;
+async function setupDatabase()  {
+    db = await open({
+        filename: './fishapp.sqlite',
+        driver: sqlite3.Database
+    });
+}
+
+setupDatabase();
+
+// Express server
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Routes
 app.get('/', (req, res) => {
     res.send('fishAPP');
 });
